4e9d9d0379366b406db39305b04cfcd0c3939006
PAUL FISMER <26086191@edgehill.ac.uk>
2025-03-18 15:19:58 +0000

    Version 1.01

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("Hello World");
         System.out.println("Hello yo");
+        String token = scanner.next();
+        System.out.println("Wow you wrote token="+token);
     }
 }
